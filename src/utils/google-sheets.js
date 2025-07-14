a8f98c64f353b986e76d6f9a0245e713c4dfc418
lokeshgandhi111 <lokeshgandhimodalavalasa111@gmail.com>
2025-07-14 09:18:49 +0530

    minute changes

--- src/utils/google-sheets.js
+++ src/utils/google-sheets.js
@@ -10,8 +10,11 @@ const credentials = {
 };
 
 const auth = new google.auth.GoogleAuth({
-  credentials,
-  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  },
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 
